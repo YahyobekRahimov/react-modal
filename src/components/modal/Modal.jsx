28ad86061c21ca64ea5869a6bf1b62aede78737d
Yahyobek Rahimov <141851971+YahyobekRahimov@users.noreply.github.com>
2024-02-03 04:15:37 +0500

    project complete

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,10 +1,24 @@
+import { useEffect } from "react";
 import "./Modal.css";
 import CloseIcon from "/src/assets/close.svg?react";
 
 export default function Modal({ setOpen, open }) {
+   function closeOnEscape(e) {
+      if (e.key === "Escape") {
+         setOpen(false);
+      }
+   }
+
+   useEffect(() => {
+      if (open) {
+         document.addEventListener("keyup", closeOnEscape);
+      } else {
+         document.removeEventListener("keyup", closeOnEscape);
+      }
+   }, [open]);
+
    function handleModalClick(e) {
       e.stopPropagation();
-      console.log("clicked");
    }
    return (
       <div
@@ -13,13 +27,14 @@ export default function Modal({ setOpen, open }) {
       >
          <div className={`modal ${open ? "fade" : ""}`}>
             <div
-               className={`modal-content`}
                onClick={handleModalClick}
+               className={`modal-content ${open ? "index-more" : ""}`}
             >
-               Lorem ipsum dolor sit amet consectetur adipisicing
-               elit. Aperiam, voluptates natus cupiditate quaerat
-               cumque minus alias dignissimos distinctio at aut.
-               <button>{<CloseIcon />}</button>
+               Click escape or the close button on top or Click
+               anything but the modal to close the modal
+               <button onClick={() => setOpen(false)}>
+                  {<CloseIcon />}
+               </button>
             </div>
          </div>
       </div>
